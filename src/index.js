45308f72ae2ee6875413e88691511253dbf2173d
Jessica <jessica@email.com>
2020-06-03 12:40:18 +0000

    List iteration

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 
-const customerButton = <button>There's a customer here!</button>;
-
-const hasCustomer = true;
+const customerList = [
+  {
+    id: 1,
+    name: 'Julia Pacheco'
+  },
+  {
+    id: 2,
+    name: 'Michael Pereira'
+  },
+  {
+    id: 3,
+    name: 'Maria Antunes'
+  }
+];
 
 const App = () => {
 
-  const renderCustomer = (
-    <Fragment>
-      {customerButton}
-    </Fragment>
-  );
-
-  const renderRegister = (
-    <Fragment>
-      <button>Register a customer</button>
-    </Fragment >
-  );
+  const renderCustomers = (customer) => {
+    return (
+      <li key={`customer-${customer.id}`}>{customer.name}</li>
+    )
+  };
 
   return (
     <div>
-      {hasCustomer ? renderCustomer : renderRegister}
+      <ul>
+        {customerList.map(renderCustomers)}
+      </ul>
     </div >
   );
-}
+};
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
